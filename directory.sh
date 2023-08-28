86f3e694d582471e7faa8567ab65aa1e4cd93104
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-27 20:25:04 -0700

    Added extra functionalitoes in 'git.sh' and 'directory.sh'

--- directory.sh
+++ directory.sh
@@ -4,8 +4,12 @@
 echo "Enter Directory Name: "
 read -r dir
 
+# Trim leading and trailing whitespace from user input
+dir="${dir#"${dir%%[![:space:]]*}"}"
+dir="${dir%"${dir##*[![:space:]]}"}"
+
 # Checks if entered string is empty.
-if [[-z $dir ]]; then	# DOuble square bracket is only for functionality.
+if [[ -z $dir ]]; then	# DOuble square bracket is only for functionality.
 	echo "Directory name cannot be empty."
 	exit 1
 fi
@@ -16,17 +20,32 @@ if [[ $dir =~ [^a-zA-Z0-9_-] ]]; then
 	exit 1
 fi
 
+#Check if directory already exosts
+if [ -d "$dir" ]; then
+	echo "Directory '$dir' already exists."
+	exit 1
+fi
+
 # Create the directory and handles error.
-mkdir "$dir" || { echo "Failed to create directory."; exit 1}
+mkdir "$dir" || { echo "Failed to create directory."; exit 1; }
 
 # Copies the script, 'git.sh' to the To-be created dirrectory/
-cp git.sh "$dir" || { echo "Failed to copy script to directory!"; exit 1}
+cp git.sh "$dir" || { echo "Failed to copy script to directory!"; exit 1; }
+
+# Copies the script, 'temp.sh' to the To-be created dirrectory/
+cp temp.sh "$dir" || { echo "Failed to copy script to directory!"; exit 1; }
 
 # Change into newly created diretory;
 cd "$dir" || { echo "Failed to change into directory."; exit 1; }
 
-# Takes input for README
-echo "Enter README: "
-read -r readme
+# Use vi to collect README input from the user
+echo "Press enter to open README in vi. Write your README and save using ':wq'"
+read -r
+vi README.md || { echo "Failed to edit README file!"; exit 1; }
 
-echo $readme > README.md || { echo "Failed to create README file!"; exit 1}
+git add README.md
+
+# Commit with a default message
+git commit -m "Added Readme" || { echo "Failed to commit changes."; exit 1; }
+
+git push
--- git.sh
+++ git.sh
@@ -7,18 +7,59 @@ if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
 	exit 1
 fi
 
-echo "Enter file name: >"
-read input
-if [ ! -f "$input" ]; then
-	echo "File does not exist!"
-	exit 1
+# Ask the  user for the commit mode
+echo "Select commit mode:"
+echo "1. Add multiple files with one commit"
+echo "2. Add different files with different commits"
+read -r commit_mode
+
+# Validate the commit mode
+if [[ "$commit_mode" != "1" && "$commit_mode" != "2" ]]; then
+	        echo "Invalid commit mode selected."
+		exit 1
 fi
 
-git add "$input" || { echo "Failed to add "$input" to git!"; exit 1; }
+# Ask user for files to add
+while true; do
+	echo "Enter a file to add (or press Enter to finish): "
+	read -r file
+	if [[ -z "$file" ]]; then
+		break
+	fi
 
-echo "Enter commit message: "
-read commit
+	# Check if the entered file exists
+	if [ ! -f "$file" ]; then
+		echo "File does not exist!"
+		continue
+	fi
+							
+	git add "$file" || { echo "Failed to stage file '$file'."; exit 1; }
 
-git commit -m "$commit" || { echo "Failed to commit changes."; exit 1; }
+	if [[ "$commit_mode" == "1" ]]; then
+		# If committing all files together, store the commit message
+		echo "Enter commit message for all files: "
+		read commit
+		break
+	else
+		 # If committing files individually, prompt for a commit message for the current file
+	echo "Enter commit message for '$file': "
+	read commit
+	# Commit the changes with the provided message for the current file
+	git commit -m "$commit" || { echo "Failed to commit changes."; exit 1; }
+	fi
+done
 
-git push || { echo "Failed to push changes."; exit 1; }
+# Commit all staged changes together with the stored commit message
+if [[ "$commit_mode" == "1" ]]; then
+	git commit -m "$commit" || { echo "Failed to commit changes."; exit 1; }
+fi
+
+# Confirm with the user before pushing
+echo "Do you want to push the changes to the remote repository? (y/n)"
+read -r confirm
+if [[ "$confirm" == "y" ]]; then
+	# Push to the remote repository
+	git push || { echo "Failed to push changes."; exit 1; }
+else
+	echo "Changes are staged but not pushed."
+fi
